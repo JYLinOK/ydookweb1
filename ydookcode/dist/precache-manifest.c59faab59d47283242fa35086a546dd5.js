1d569c1df811105b52adef648ec4d170dc33c0fb
YDOOK <35529779+JYLinOK@users.noreply.github.com>
2021-12-06 11:12:10 +0800

    Jinwei Lin

---
+++ ydookcode/dist/precache-manifest.c59faab59d47283242fa35086a546dd5.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a324dc69ad7f861f9d4330cfe4053f43",
+    "url": "/PhotoD/PicSet/1.jpg"
+  },
+  {
+    "revision": "98715175461a3a8faf45527185fe0fdd",
+    "url": "/PhotoD/PicSet/10.jpg"
+  },
+  {
+    "revision": "10dc2ae1b724c05491b8fc75ef16d110",
+    "url": "/PhotoD/PicSet/11.jpg"
+  },
+  {
+    "revision": "020b6e849aabd157584842212603409c",
+    "url": "/PhotoD/PicSet/12.jpg"
+  },
+  {
+    "revision": "55890e0a93e3a24b8df3d2af199a8e54",
+    "url": "/PhotoD/PicSet/13.jpg"
+  },
+  {
+    "revision": "8380a41d3cd2a02c667e68d6ac73f78f",
+    "url": "/PhotoD/PicSet/14.jpg"
+  },
+  {
+    "revision": "6693c15a93e09dd7cf944cbc12f600ab",
+    "url": "/PhotoD/PicSet/15.jpg"
+  },
+  {
+    "revision": "ae723a588df8ac512dbf55e15340a70f",
+    "url": "/PhotoD/PicSet/16.jpg"
+  },
+  {
+    "revision": "a2a96b407567091612d72933454b652a",
+    "url": "/PhotoD/PicSet/17.jpg"
+  },
+  {
+    "revision": "dd300c720dd65694d93844e55b213f13",
+    "url": "/PhotoD/PicSet/18.jpg"
+  },
+  {
+    "revision": "b8a7bfc8e140c3c91fcb427899ca4f19",
+    "url": "/PhotoD/PicSet/19.jpg"
+  },
+  {
+    "revision": "0b52da6235c4227322e835fd4f8bdf41",
+    "url": "/PhotoD/PicSet/2.jpg"
+  },
+  {
+    "revision": "51fc709c88bad1a3fb32ab10a4a1e3f6",
+    "url": "/PhotoD/PicSet/20.jpg"
+  },
+  {
+    "revision": "72ff691fd4724cf2c1dff74144916c69",
+    "url": "/PhotoD/PicSet/3.jpg"
+  },
+  {
+    "revision": "7d971f6c40c289734a59d2871b983f44",
+    "url": "/PhotoD/PicSet/4.jpg"
+  },
+  {
+    "revision": "a6596a1c353d85bb36ed28f8b139c9b5",
+    "url": "/PhotoD/PicSet/5.jpg"
+  },
+  {
+    "revision": "dd0e15ddff3a35d3b6212cb4de4d2adb",
+    "url": "/PhotoD/PicSet/6.jpg"
+  },
+  {
+    "revision": "3a2ceee9f2a951430702f158f764cb07",
+    "url": "/PhotoD/PicSet/7.jpg"
+  },
+  {
+    "revision": "bc6958fc5f5fbe221c5912383819d58b",
+    "url": "/PhotoD/PicSet/8.jpg"
+  },
+  {
+    "revision": "4377f0ef24174e1e714d408fa09e2d7f",
+    "url": "/PhotoD/PicSet/9.jpg"
+  },
+  {
+    "revision": "483da4a653f854a6233bc65912ede8f5",
+    "url": "/PhotoD/wisdom.jpg"
+  },
+  {
+    "revision": "aeab70ffb1ee9836b0a03885e9f51f3d",
+    "url": "/PhotoEditor/Editor.jpg"
+  },
+  {
+    "revision": "aaa25b05e9d9550030fc928ba37d34a0",
+    "url": "/PhotoO1/ExpandAcademic.jpg"
+  },
+  {
+    "revision": "5a8fd71b5120084eb26389315e61361b",
+    "url": "/PhotoO2/OpenSource.jpg"
+  },
+  {
+    "revision": "75d1b0adf9019de913dcb7ec8220efdf",
+    "url": "/PhotoO2/Softwares/PyDraw.jpg"
+  },
+  {
+    "revision": "965edb8c2e85eacdf8ece1437416636b",
+    "url": "/PhotoO2/Softwares/YWeb.jpg"
+  },
+  {
+    "revision": "e56cae13edd3b8f92e9a8371a90abaad",
+    "url": "/PhotoO2/Softwares/YecPad.jpg"
+  },
+  {
+    "revision": "1359ac6482a6ba6eb26d0663cfa05542",
+    "url": "/PhotoO2/Softwares/YecPen.jpg"
+  },
+  {
+    "revision": "6605bc754110f581576298938ef14435",
+    "url": "/PhotoO2/Softwares/YecText.jpg"
+  },
+  {
+    "revision": "4feffcc260941af14dc87066593fc4ce",
+    "url": "/PhotoY/JY.png"
+  },
+  {
+    "revision": "0448f9e540cd71fe53bfa6af6690a51a",
+    "url": "/PhotoY/author.jpg"
+  },
+  {
+    "revision": "83bc9665d715da1dda484b27e543026d",
+    "url": "/PhotoY/home.jpg"
+  },
+  {
+    "revision": "ef8f85f185690228a826cfe5b56364fa",
+    "url": "/PhotoY/introduction.jpg"
+  },
+  {
+    "revision": "5fa24983e0fc23d2884458d0e4e4d9a0",
+    "url": "/PhotoY/vison.jpg"
+  },
+  {
+    "revision": "b494e29c1c4832a88b19",
+    "url": "/css/app.25303958.css"
+  },
+  {
+    "revision": "a324dc69ad7f861f9d4330cfe4053f43",
+    "url": "/img/1.a324dc69.jpg"
+  },
+  {
+    "revision": "98715175461a3a8faf45527185fe0fdd",
+    "url": "/img/10.98715175.jpg"
+  },
+  {
+    "revision": "10dc2ae1b724c05491b8fc75ef16d110",
+    "url": "/img/11.10dc2ae1.jpg"
+  },
+  {
+    "revision": "020b6e849aabd157584842212603409c",
+    "url": "/img/12.020b6e84.jpg"
+  },
+  {
+    "revision": "55890e0a93e3a24b8df3d2af199a8e54",
+    "url": "/img/13.55890e0a.jpg"
+  },
+  {
+    "revision": "8380a41d3cd2a02c667e68d6ac73f78f",
+    "url": "/img/14.8380a41d.jpg"
+  },
+  {
+    "revision": "6693c15a93e09dd7cf944cbc12f600ab",
+    "url": "/img/15.6693c15a.jpg"
+  },
+  {
+    "revision": "ae723a588df8ac512dbf55e15340a70f",
+    "url": "/img/16.ae723a58.jpg"
+  },
+  {
+    "revision": "a2a96b407567091612d72933454b652a",
+    "url": "/img/17.a2a96b40.jpg"
+  },
+  {
+    "revision": "dd300c720dd65694d93844e55b213f13",
+    "url": "/img/18.dd300c72.jpg"
+  },
+  {
+    "revision": "b8a7bfc8e140c3c91fcb427899ca4f19",
+    "url": "/img/19.b8a7bfc8.jpg"
+  },
+  {
+    "revision": "0b52da6235c4227322e835fd4f8bdf41",
+    "url": "/img/2.0b52da62.jpg"
+  },
+  {
+    "revision": "51fc709c88bad1a3fb32ab10a4a1e3f6",
+    "url": "/img/20.51fc709c.jpg"
+  },
+  {
+    "revision": "72ff691fd4724cf2c1dff74144916c69",
+    "url": "/img/3.72ff691f.jpg"
+  },
+  {
+    "revision": "7d971f6c40c289734a59d2871b983f44",
+    "url": "/img/4.7d971f6c.jpg"
+  },
+  {
+    "revision": "a6596a1c353d85bb36ed28f8b139c9b5",
+    "url": "/img/5.a6596a1c.jpg"
+  },
+  {
+    "revision": "dd0e15ddff3a35d3b6212cb4de4d2adb",
+    "url": "/img/6.dd0e15dd.jpg"
+  },
+  {
+    "revision": "3a2ceee9f2a951430702f158f764cb07",
+    "url": "/img/7.3a2ceee9.jpg"
+  },
+  {
+    "revision": "bc6958fc5f5fbe221c5912383819d58b",
+    "url": "/img/8.bc6958fc.jpg"
+  },
+  {
+    "revision": "4377f0ef24174e1e714d408fa09e2d7f",
+    "url": "/img/9.4377f0ef.jpg"
+  },
+  {
+    "revision": "aaa25b05e9d9550030fc928ba37d34a0",
+    "url": "/img/ExpandAcademic.aaa25b05.jpg"
+  },
+  {
+    "revision": "4feffcc260941af14dc87066593fc4ce",
+    "url": "/img/JY.4feffcc2.png"
+  },
+  {
+    "revision": "5a8fd71b5120084eb26389315e61361b",
+    "url": "/img/OpenSource.5a8fd71b.jpg"
+  },
+  {
+    "revision": "75d1b0adf9019de913dcb7ec8220efdf",
+    "url": "/img/PyDraw.75d1b0ad.jpg"
+  },
+  {
+    "revision": "965edb8c2e85eacdf8ece1437416636b",
+    "url": "/img/YWeb.965edb8c.jpg"
+  },
+  {
+    "revision": "e56cae13edd3b8f92e9a8371a90abaad",
+    "url": "/img/YecPad.e56cae13.jpg"
+  },
+  {
+    "revision": "1359ac6482a6ba6eb26d0663cfa05542",
+    "url": "/img/YecPen.1359ac64.jpg"
+  },
+  {
+    "revision": "6605bc754110f581576298938ef14435",
+    "url": "/img/YecText.6605bc75.jpg"
+  },
+  {
+    "revision": "0448f9e540cd71fe53bfa6af6690a51a",
+    "url": "/img/author.0448f9e5.jpg"
+  },
+  {
+    "revision": "83bc9665d715da1dda484b27e543026d",
+    "url": "/img/home.83bc9665.jpg"
+  },
+  {
+    "revision": "ef8f85f185690228a826cfe5b56364fa",
+    "url": "/img/introduction.ef8f85f1.jpg"
+  },
+  {
+    "revision": "5fa24983e0fc23d2884458d0e4e4d9a0",
+    "url": "/img/vison.5fa24983.jpg"
+  },
+  {
+    "revision": "483da4a653f854a6233bc65912ede8f5",
+    "url": "/img/wisdom.483da4a6.jpg"
+  },
+  {
+    "revision": "a572cc1555bc6a3f2ddd73fcb87eb8a2",
+    "url": "/img/ydook.a572cc15.jpg"
+  },
+  {
+    "revision": "3a7087e55f16832c954c90ba02f7f49c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b494e29c1c4832a88b19",
+    "url": "/js/app.92857909.js"
+  },
+  {
+    "revision": "66cd1938b0d8ff939768",
+    "url": "/js/chunk-vendors.e3ce8e38.js"
+  },
+  {
+    "revision": "aaab6ec141b42242e529e68d07d5a193",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "6ebf9d5ac452dea0e2b3d8de865474cb",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a572cc1555bc6a3f2ddd73fcb87eb8a2",
+    "url": "/ydook.ico"
+  },
+  {
+    "revision": "a572cc1555bc6a3f2ddd73fcb87eb8a2",
+    "url": "/ydook.jpg"
+  }
+]);
